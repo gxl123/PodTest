3baa61c0e1e85a6e77bd6b53437f54a8fb77ab89
gxl <2180877579@qq.com>
2018-04-08 18:26:40 +0800

    Init commit

---
+++ PodTest/PodTest.h
@@ -0,0 +1,13 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by gxl on 2018/4/8.
+//  Copyright © 2018年 gxl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTest : NSObject
++(void)test;
+@end
---
+++ PodTest/PodTest.m
@@ -0,0 +1,15 @@
+//
+//  PodTest.m
+//  PodTest
+//
+//  Created by gxl on 2018/4/8.
+//  Copyright © 2018年 gxl. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
++(void)test {
+    NSLog(@"hello world");
+}
+@end
